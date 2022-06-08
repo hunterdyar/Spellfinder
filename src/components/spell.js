3a684cac1438720dadcb7f9a3bf86f56ca904295
Hunter Dyar <bloop@hdyar.com>
2022-06-08 16:23:58 -0400

    Uses react-markdown for spell description rendering. Some spells are in markdown. react-gfm for table support.

--- src/components/spell.js
+++ src/components/spell.js
@@ -1,11 +1,16 @@
 import {Box, Card, Container, Divider} from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
 
 function SpellView({spell}){
     if(typeof spell === "undefined")
     {
         return <Card>
             <div style={{opacity: 0.2}}>
-                <h1>No Spell Selected</h1>
+                <h2>No Spell Selected</h2>
+                <Box style={{width: "100%"}}>
+                <Divider />
+                </Box>
             </div>
         </Card>
     }else {
@@ -16,7 +21,10 @@ function SpellView({spell}){
                 <Container align={"left"}>
                 <p><strong>Casting Time: </strong><span>{spell.casting_time}</span></p>
                 <Box>
-                    {spell.desc.map(d=> <p key={d}>{d}</p>)}
+                    {/*{spell.desc.map(d=> <p key={d}>{d}</p>)}*/}
+                    <ReactMarkdown children={spell.desc.join('  \n')}
+                                   remarkPlugins={[remarkGfm]}
+                    />
                 </Box>
                     {/*Todo: Damage with damage level dropdown*/}
                     {/*Todo: Area of Effect Component*/}
